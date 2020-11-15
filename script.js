72ab887f17c5bc46972bdb482f4293b1fc219bcd
shokoufe <45529345+shokoghasemi@users.noreply.github.com>
2020-11-15 14:34:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
@@ -12,7 +14,7 @@ for (var i in names) {
   if (firstLetter === 'j') {
     window.byeSpeaker.speak(names[i]);
   } else {
-    window.helloSpeaker.speak(names[i]);
+    window.helloSpeaker.(speak(names[i]));
   }
 }
 
