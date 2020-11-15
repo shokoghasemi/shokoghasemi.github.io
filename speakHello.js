a467a7eb90728ca718a4b00d063d92a5b53a2282
shokoufe <45529345+shokoghasemi@users.noreply.github.com>
2020-11-15 14:35:37 +0330

    Update speakHello.js

--- speakHello.js
+++ speakHello.js
@@ -5,9 +5,9 @@ var helloSpeaker;
  
 var speakWord = "Hello";
  
-helloSpeaker.speak = function (name) {
+helloSpeaker.speak = (function (name) {
   console.log(speakWord + " " + name);
-}
+})
  
 window.helloSpeaker = helloSpeaker;
 })(window);
